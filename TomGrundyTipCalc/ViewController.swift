62f57df8aa17d1d6e9bd1d9a6aca372d73da6935
Thomas Grundy <thomas.g@irokopartners.com>
2017-07-23 12:45:50 -0400

    Initial Commit

---
+++ TomGrundyTipCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TomGrundyTipCalc
+//
+//  Created by Tom Grundy on 7/23/17.
+//  Copyright © 2017 Tom Grundy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
